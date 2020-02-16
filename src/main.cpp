8be15d109988ea1181dd8d89e0ca3408e595aff1
Gleb K <glebgkom@gmail.com>
2020-02-16 04:20:59 +0300

    Ардуина теперь ждет подключения, после чего шлет координаты. Подключены внешние библиотеки

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,53 @@
 #include <Arduino.h>
-// #include <string.h>
+#include <ATimer.h>
+#include <keywords.h>
+
+ATimer connectionTimer,
+       positionUpdateTimer;
+
+bool connected = false;
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(connnectionSpeed);
+    connectionTimer.setPeriod(1000);
+    connectionTimer.start();
+    positionUpdateTimer.setPeriod(200);
+    positionUpdateTimer.start();
 }
 
 void loop() {
-    String str = "current time: ";
-    str += String(millis()/1000.0,1);
-    // char* message = "qwerty";
-    // str += message;
-    // Serial.write(str.c_str());
-    Serial.println(str);
-    delay(1417);
+
+    if (connectionTimer.check() && !connected) {
+        Serial.println(movingCamId);
+
+        if (Serial.find(connectRequest)){
+            Serial.println(connectApprove);
+            connected = true;
+            connectionTimer.stop();
+        }
+    }
+
+    if (positionUpdateTimer.check() && connected) {
+        char *q = "NEW_POS ";
+        String message(q);
+        message += String(rand());
+        message += ' ';
+        message += String(rand());
+
+        Serial.println(message);
+    }
 }
+
+// void serialEvent() {
+//   while (Serial.available()) {
+//     // get the new byte:
+//     char inChar = (char)Serial.read();
+//     // add it to the inputString:
+//     inputString += inChar;
+//     // if the incoming character is a newline, set a flag so the main loop can
+//     // do something about it:
+//     if (inChar == '\n') {
+//       stringComplete = true;
+//     }
+//   }
+// }
